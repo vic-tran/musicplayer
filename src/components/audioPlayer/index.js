2398b78b187b6c5ae55a61d827398b2bb193407f
Victor Tran <vc.tran998@gmail.com>
2022-05-14 14:16:55 -0700

    Made wave animation

--- src/components/audioPlayer/index.js
+++ src/components/audioPlayer/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import "./audioPlayer.css";
+import Controls from './controls';
 import ProgressCircle from "./progressCircle";
+import WaveAnimation from './waveAnimation';
 
-export default function AudioPlayer({ currentTrack }) {
+export default function AudioPlayer({ currentTrack, isPlaying }) {
+
+    /* create artist constant to use for player right body*/
+    const artists = [];
+    currentTrack?.album?.artists.forEach((artist) => {
+        artists.push(artist.name);
+    });
   return (
     <div className="player-body flex">
         <div className="player-left-body">
@@ -14,7 +22,24 @@ export default function AudioPlayer({ currentTrack }) {
               color="#C96850"
            />
         </div>
-        <div className="player-right-body"></div>
+        <div className="player-right-body flex">
+            <p className="song-title">{currentTrack?.name}</p>
+            <p className="song-artist">{artists.join(" | ")}</p>
+            <div className="player-right-bottom flex">
+                <div className="song-duration flex">
+                    <p className="duration">0:01</p>
+                    <WaveAnimation isPlaying={true}/>
+                    <p className="duration">0:30</p>
+                </div>
+            <Controls 
+                // isPlaying={isPlaying}
+                // setIsPlaying={setIsPlaying}
+                // handleNext={handleNext}
+                // handlePrev={handlePrev}
+                // total={total}
+            />
+            </div>
+        </div>
     </div>
   );
 }
